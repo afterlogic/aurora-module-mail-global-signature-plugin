ee615b50f240a0d394859d20b40c60c25c21d29c
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:57 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ namespace Aurora\Modules\MailGlobalSignaturePlugin;
 
 /**
  * With this plugin enabled and configured, all email messages sent out will have a predefined message appended.
- * 
+ *
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2023, Afterlogic Corp.
@@ -18,25 +18,25 @@ namespace Aurora\Modules\MailGlobalSignaturePlugin;
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	/**
-	 * @var \Aurora\Modules\Mail\Module
-	 */
-	protected $oMailModule;
-	
-	public function init() 
-	{
-		$this->oMailModule = \Aurora\System\Api::GetModule('Mail');
-	
-		$this->subscribeEvent('Mail::SendMessage::before', array($this, 'onBeforeSendMessage'));
-	}
-	
-	/**
-	 * 
-	 * @param array $aArguments
-	 * @param mixed $mResult
-	 */
-	public function onBeforeSendMessage(&$aArguments, &$mResult)
-	{
-		$aArguments["Text"] .= ($aArguments["IsHtml"] ? "<br />":"\r\n") . $this->getConfig('Signature','');
-	}
+    /**
+     * @var \Aurora\Modules\Mail\Module
+     */
+    protected $oMailModule;
+
+    public function init()
+    {
+        $this->oMailModule = \Aurora\System\Api::GetModule('Mail');
+
+        $this->subscribeEvent('Mail::SendMessage::before', array($this, 'onBeforeSendMessage'));
+    }
+
+    /**
+     *
+     * @param array $aArguments
+     * @param mixed $mResult
+     */
+    public function onBeforeSendMessage(&$aArguments, &$mResult)
+    {
+        $aArguments["Text"] .= ($aArguments["IsHtml"] ? "<br />" : "\r\n") . $this->getConfig('Signature', '');
+    }
 }
